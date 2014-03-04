6ac700d9f82deee64e72942ea1b158e93a30ccba
A.Matsukura <jack.knife.am@gmail.com>
2014-03-04 10:55:36 +0900

    u23 package.el add / init.el modify

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;;Last Change:2014/03/03 13:36.
+;;;Last Change:2014/03/04 10:46.
 
 (require 'cl)
 
@@ -6,12 +6,36 @@
 (setq user-mail-address (getenv "MAILADDRESS"))
 (setq system-name (getenv "COMPANY"))
 
+;; system-type predicates
+(setq 
+	  emacs23-p (<= emacs-major-version 23)
+	  emacs24-p (>= emacs-major-version 24)
+	  darwin-p (eq system-type 'darwin)
+      ns-p (featurep 'ns)
+      carbon-p (eq window-system 'mac)
+      linux-p (eq system-type 'gnu/linux)
+      colinux-p (when linux-p
+                  (let ((file "/proc/modules"))
+                    (and
+                     (file-readable-p file)
+                     (x->bool
+                      (with-temp-buffer
+                        (insert-file-contents file)
+                        (goto-char (point-min))
+                        (re-search-forward "^cofuse\.+" nil t))))))
+      cygwin-p (eq system-type 'cygwin)
+      nt-p (eq system-type 'windows-nt)
+      meadow-p (featurep 'meadow)
+      windows-p (or cygwin-p nt-p meadow-p))
+
 ;;Load-path
 (setq load-path (cons   "~/.emacs.d/elisp" load-path)) 
 
 ;;ELPA
 ;; package.el
-;;(setq load-path (cons   "~/.emacs.d/elisp/u23" load-path)) 
+(when emacs23-p
+  (setq load-path (cons   "~/.emacs.d/elisp/u23" load-path)) 
+)
 
 (require 'package)
 (setq package-user-dir "~/.emacs.d/elisp/elpa") 
@@ -34,19 +58,19 @@
 	evil-numbers
 	evil-paredit
 	evil-tabs
-	flycheck
+	;flycheck
     helm
     helm-ag
     helm-descbinds
     helm-ls-git
     init-loader
-	jedi
-    js2-mode
-    magit
-    markdown-mode
+	;jedi
+    ;js2-mode
+    ;magit
+    ;markdown-mode
     open-junk-file
     recentf-ext
-    ruby-mode
+    ;ruby-mode
 	quickrun
 	w3m
 	web-mode
@@ -62,24 +86,27 @@
     (dolist (package not-installed)
       (package-install package))))
 
-;; system-type predicates
-(setq darwin-p (eq system-type 'darwin)
-      ns-p (featurep 'ns)
-      carbon-p (eq window-system 'mac)
-      linux-p (eq system-type 'gnu/linux)
-      colinux-p (when linux-p
-                  (let ((file "/proc/modules"))
-                    (and
-                     (file-readable-p file)
-                     (x->bool
-                      (with-temp-buffer
-                        (insert-file-contents file)
-                        (goto-char (point-min))
-                        (re-search-forward "^cofuse\.+" nil t))))))
-      cygwin-p (eq system-type 'cygwin)
-      nt-p (eq system-type 'windows-nt)
-      meadow-p (featurep 'meadow)
-      windows-p (or cygwin-p nt-p meadow-p))
+;;u23 error package
+(when emacs24-p
+(defvar my-package-list2
+  '(
+	flycheck
+	jedi
+    js2-mode
+    magit
+    markdown-mode
+    ruby-mode
+	))
+
+(let ((not-installed
+       (loop for package in my-package-list2
+             when (not (package-installed-p package))
+             collect package)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (package not-installed)
+      (package-install package))))
+)
 
 ;;init-loader
 (require 'init-loader)
