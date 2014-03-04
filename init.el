c594cf61a2990e3ed61e0a2a52ad7882278f85f1
A.Matsukura <jack.knife.am@gmail.com>
2014-03-04 11:04:41 +0900

    init.el colinux-p delete

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;;Last Change:2014/03/04 10:46.
+;;;Last Change:2014/03/04 11:04.
 
 (require 'cl)
 
@@ -14,15 +14,15 @@
       ns-p (featurep 'ns)
       carbon-p (eq window-system 'mac)
       linux-p (eq system-type 'gnu/linux)
-      colinux-p (when linux-p
-                  (let ((file "/proc/modules"))
-                    (and
-                     (file-readable-p file)
-                     (x->bool
-                      (with-temp-buffer
-                        (insert-file-contents file)
-                        (goto-char (point-min))
-                        (re-search-forward "^cofuse\.+" nil t))))))
+;      colinux-p (when linux-p
+;                  (let ((file "/proc/modules"))
+;                    (and
+;                     (file-readable-p file)
+;                     (x->bool
+;                      (with-temp-buffer
+;                        (insert-file-contents file)
+;                        (goto-char (point-min))
+;                        (re-search-forward "^cofuse\.+" nil t))))))
       cygwin-p (eq system-type 'cygwin)
       nt-p (eq system-type 'windows-nt)
       meadow-p (featurep 'meadow)
@@ -58,19 +58,13 @@
 	evil-numbers
 	evil-paredit
 	evil-tabs
-	;flycheck
     helm
     helm-ag
     helm-descbinds
     helm-ls-git
     init-loader
-	;jedi
-    ;js2-mode
-    ;magit
-    ;markdown-mode
     open-junk-file
     recentf-ext
-    ;ruby-mode
 	quickrun
 	w3m
 	web-mode
