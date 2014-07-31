b754de9e77330a1d3aef27e17415e65310867f76
matsukura <matsukura@zenic.co.jp>
2014-07-31 14:43:15 +0900

    update

--- inits/23-edit_mode_verilog.el
+++ inits/23-edit_mode_verilog.el
@@ -18,22 +18,39 @@
 	verilog-indent-level             4
 	verilog-indent-level-module      0
 	verilog-indent-level-declaration 0
-	verilog-indent-level-behavioral  0   ;;task/function indent
+	verilog-indent-level-behavioral  4   ;;function indent
 	verilog-indent-level-directive   0
 	verilog-case-indent              4
-	verilog-cexp-indent				 4	
+	verilog-cexp-indent				 0	
 	verilog-auto-newline             nil
 	verilog-auto-indent-on-newline   nil
-	verilog-tab-always-indent        nil
+	verilog-tab-always-indent        t
 	verilog-auto-endcomments         t
-	verilog-minimum-comment-distance 40
+;;begin-endが何行はなれていたらendコメントを付加するか
+	verilog-minimum-comment-distance 10
 	verilog-indent-begin-after-if    t
-	verilog-auto-lineup              nil
+	verilog-auto-lineup              `all
 	verilog-indent-lists             t
 	verilog-highlight-modules        t
 	verilog-highlight-p1800-keywords nil
 	verilog-highlight-grouping-keywords t
 	verilog-highlight-includes       t
-	verilog-date-scientific-format t
+	verilog-date-scientific-format   t
+;;
+    ;;tabしたときに自動でコメントアウトを付加 要tab-mode t
+	verilog-tab-to-comment nil
+    ;;
+	verilog-align-ifelse nil
+	verilog-auto-declare-nettype nil
+	verilog-auto-wire-type t
+	verilog-assignment-delay "#(2)"
+	verilog-auto-inst-param-value nil
+	verilog-auto-inst-column 40
+	verilog-auto-inst-interfaced-ports nil
+	verilog-auto-input-ignore-regexp nil
+	verilog-auto-inout-ignore-regexp nil
+	verilog-auto-template-warn-unused nil
+	verilog-auto-tieoff-declaration "wire"
+;;
 	verilog-company (getenv "COMPANY")
 )	
